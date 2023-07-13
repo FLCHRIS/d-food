3d78398e0e96b618cb32c00cf1f82246257e6b2c
Christian <filetelopezchristian@gmail.com>
2023-07-13 18:25:53 -0500

    Se agregó la funcionalidad de tema dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        Black: '#040505',
+        White: '#FFFFFF',
+        WhiteLight: '#8E98A6'
+      },
+      fontFamily: {
+        Roboto: ['Roboto', 'sans-serif']
+      }
+    }
   },
   plugins: []
 }
