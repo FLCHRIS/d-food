4d3b267bab00f53666b62b612bbca1786483b372
Christian <filetelopezchristian@gmail.com>
2023-07-14 17:38:38 -0500

    Se agregó un prop para mejorar las animaciones

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -3,10 +3,10 @@ import 'aos/dist/aos.css'
 import PropTypes from 'prop-types'
 import AOS from 'aos'
 
-export const Clients = ({ paragraph, name, job, image }) => {
+export const Clients = ({ paragraph, name, job, image, animation }) => {
   AOS.init()
   return (
-    <div data-aos='fade-up' data-aos-duration='1000' data-aos-anchor-placement='bottom-center' className='border-[.1rem] flex flex-col justify-between border-Black border-dashed p-[1rem] w-full mx-auto max-w-sm dark:border-White transition-colors'>
+    <div data-aos='fade-up' data-aos-duration='1000' data-aos-delay={animation} className='border-[.1rem] flex flex-col justify-between border-Black border-dashed p-[1rem] w-full mx-auto max-w-sm dark:border-White transition-colors'>
       <p className='font-Roboto text-Black dark:text-White transition-colors text-center'>{paragraph}</p>
       <div>
         <img src={image} alt='' className='w-[3.5rem] h-[3.5rem] object-cover mx-auto mt-[2rem]' />
@@ -25,5 +25,6 @@ Clients.propTypes = {
   paragraph: PropTypes.string,
   name: PropTypes.string,
   job: PropTypes.string,
-  image: PropTypes.string
+  image: PropTypes.string,
+  animation: PropTypes.string
 }
