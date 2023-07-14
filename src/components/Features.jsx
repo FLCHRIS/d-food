5e79538869782225be614e986800aa58a8bc87ac
Christian <filetelopezchristian@gmail.com>
2023-07-14 17:38:46 -0500

    Se agregó un prop para mejorar las animaciones

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
-export const Features = ({ paragraph, title }) => {
+export const Features = ({ paragraph, title, animation }) => {
   AOS.init()
   return (
-    <div data-aos='fade-up' data-aos-duration='1000' className='border-[.1rem] border-Black border-dashed p-[1rem] w-full mx-auto max-w-sm dark:border-White transition-colors'>
+    <div data-aos='fade-up' data-aos-duration='1000' data-aos-delay={animation} className='border-[.1rem] border-Black border-dashed p-[1rem] w-full mx-auto max-w-sm dark:border-White transition-colors'>
       <img src={iconSalad} alt='' aria-hidden='true' className='dark:invert' />
       <h3 className='font-Roboto font-semibold text-Black my-[.8rem] dark:text-White transition-colors'>{title}</h3>
       <p className='font-Roboto text-Black dark:text-White transition-colors'>
@@ -19,5 +19,6 @@ export const Features = ({ paragraph, title }) => {
 
 Features.propTypes = {
   paragraph: PropTypes.string,
-  title: PropTypes.string
+  title: PropTypes.string,
+  animation: PropTypes.string
 }
