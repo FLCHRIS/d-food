3948a23f0690ef639bc2a5bbd199266e284335ba
Christian <filetelopezchristian@gmail.com>
2023-07-14 13:42:16 -0500

    Se agregó accesibilidad

---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ProptTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+
+export const Nav = ({ menu }) => {
+  return (
+    <nav id='menu-list' className={menu === true ? 'fixed left-0 z-10 top-0 backdrop-blur-sm w-full h-full grid place-content-center sm:grid-flow-col sm:static' : 'hidden sm:block'}>
+      <ul className='grid place-content-center gap-[2rem] sm:gap-[1rem] text-center sm:grid-flow-col'>
+        <li>
+          <NavLink to='/' className={({ isActive }) => isActive === true ? 'font-Roboto font-bold border-b-[.1rem] border-dashed border-Black p-[.1rem] text-[1.25rem] dark:text-White dark:border-White transition-colors sm:text-[1rem] sm:font-normal' : 'font-Roboto font-bold p-[.1rem] text-[1.25rem] dark:text-White dark:border-White transition-colors sm:text-[1rem] sm:font-normal'}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='menu' className={({ isActive }) => isActive === true ? 'font-Roboto font-bold border-b-[.1rem] border-dashed border-Black p-[.1rem] text-[1.25rem] dark:text-White dark:border-White transition-colors sm:text-[1rem] sm:font-normal' : 'font-Roboto font-bold p-[.1rem] text-[1.25rem] dark:text-White dark:border-White transition-colors sm:text-[1rem] sm:font-normal'}>
+            Menu
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='contact' className={({ isActive }) => isActive === true ? 'font-Roboto font-bold border-b-[.1rem] border-dashed border-Black p-[.1rem] text-[1.25rem] dark:text-White dark:border-White transition-colors sm:text-[1rem] sm:font-normal' : 'font-Roboto font-bold p-[.1rem] text-[1.25rem] dark:text-White dark:border-White transition-colors sm:text-[1rem] sm:font-normal'}>
+            Contact
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='about' className={({ isActive }) => isActive === true ? 'font-Roboto font-bold border-b-[.1rem] border-dashed border-Black p-[.1rem] text-[1.25rem] dark:text-White dark:border-White transition-colors sm:text-[1rem] sm:font-normal' : 'font-Roboto font-bold p-[.1rem] text-[1.25rem] dark:text-White dark:border-White transition-colors sm:text-[1rem] sm:font-normal'}>
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+Nav.propTypes = {
+  menu: ProptTypes.bool
+}
