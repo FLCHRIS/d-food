2a044ded86fba40c3f665fb515d727b79aed0bea
Christian <filetelopezchristian@gmail.com>
2023-07-15 11:19:51 -0500

    Se cambio el tamaño de fuente en la media screen 'md'

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -9,8 +9,8 @@ export const Features = ({ paragraph, title, animation }) => {
   return (
     <div data-aos='fade-up' data-aos-duration='1000' data-aos-delay={animation} className='border-[.1rem] border-Black border-dashed p-[1rem] w-full mx-auto max-w-sm dark:border-White transition-colors'>
       <img src={iconSalad} alt='' aria-hidden='true' className='dark:invert' />
-      <h3 className='font-Roboto font-semibold text-Black my-[.8rem] dark:text-White transition-colors'>{title}</h3>
-      <p className='font-Roboto text-Black dark:text-White transition-colors'>
+      <h3 className='font-Roboto md:text-lg font-semibold text-Black my-[.8rem] dark:text-White transition-colors'>{title}</h3>
+      <p className='font-Roboto text-Black md:text-lg dark:text-White transition-colors'>
         {paragraph}
       </p>
     </div>
