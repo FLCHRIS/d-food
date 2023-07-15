57d3aee855e14a9f8fcfa6a89ae7b30a59ab87e9
Christian <filetelopezchristian@gmail.com>
2023-07-15 11:21:11 -0500

    Se agregó el componente About

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Layout } from '../layout/Layout'
-import { Main } from '../components/Main'
+import { About } from '../pages/About'
+import { Main } from '../pages/Main'
 
 export const Router = () => {
   return (
@@ -9,7 +10,7 @@ export const Router = () => {
       <Route path='/' element={<Layout />}>
         <Route index element={<Main />} />
         <Route path='menu' element={<h1>menu</h1>} />
-        <Route path='about' element={<h1>about</h1>} />
+        <Route path='about' element={<About />} />
         <Route path='contact' element={<h1>contact</h1>} />
       </Route>
     </Routes>
